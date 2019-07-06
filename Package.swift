78b578b0bd51deea60ebb9086833f62b9f9ac297
Jeff Lett <jeffctown@gmail.com>
2019-07-05 20:46:44 -0400

    Updating private package to use ssh.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         
         .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0"),
-        .package(url: "https://github.com/jeffctown/XcodeReleasesKit.git", .branch("master"))
+        .package(url: "git@github.com:jeffctown/XcodeReleasesKit.git", .branch("master"))
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "JWT", "XcodeReleasesKit"]),
