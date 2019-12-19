7f3566770afdc5f8b6ee9e58d38e56ebc0c33267
Jeff Lett <jeffctown@gmail.com>
2019-12-19 08:34:50 -0500

    Changing APNS integration.

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         
-        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0"),
-        .package(url: "git@github.com:jeffctown/XcodeReleasesKit.git", .branch("master"))
+        .package(url: "git@github.com:jeffctown/XcodeReleasesKit.git", .branch("master")),
+	.package(url: "https://github.com/kylebrowning/APNSwift.git", from: "1.7.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "JWT", "XcodeReleasesKit"]),
