3732e07b8846b35e296e90fee37bf0d7d6bebee1
Jeff Lett <jeffctown@gmail.com>
2019-12-28 06:38:10 -0500

    Removing environment from duplicate prevention.

--- Sources/App/Controllers/DeviceController.swift
+++ Sources/App/Controllers/DeviceController.swift
@@ -7,7 +7,6 @@ final class DeviceController {
         return try req.content.decode(Device.self).flatMap(to: Device.self) { device in
             return Device.query(on: req)
                 .filter(\Device.token, .equal, device.token)
-                .filter(\Device.environment, .equal, device.environment)
                 .first()
                 .flatMap { (existing) -> EventLoopFuture<Device> in
                 if let existingDevice = existing {
