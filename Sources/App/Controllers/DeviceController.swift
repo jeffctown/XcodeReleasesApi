c7ffdc3a0773e5d242d679a786bda06e52a95c6d
Jeff Lett <jeffctown@gmail.com>
2019-12-28 05:29:10 -0500

    Preventing duplicate devices.  Devices are unique on environment and token keys.

--- Sources/App/Controllers/DeviceController.swift
+++ Sources/App/Controllers/DeviceController.swift
@@ -5,7 +5,19 @@ final class DeviceController {
     
     func create(_ req: Request) throws -> Future<Device> {
         return try req.content.decode(Device.self).flatMap(to: Device.self) { device in
-            return device.save(on: req)
+            return Device.query(on: req)
+                .filter(\Device.token, .equal, device.token)
+                .filter(\Device.environment, .equal, device.environment)
+                .first()
+                .flatMap { (existing) -> EventLoopFuture<Device> in
+                if let existingDevice = existing {
+                    existingDevice.type = device.type
+                    existingDevice.environment = device.environment
+                    return existingDevice.update(on: req)
+                } else {
+                    return device.save(on: req)
+                }
+            }
         }
     }
     
